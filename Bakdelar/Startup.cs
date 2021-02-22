82de1d6b9cd8c08e1b0f3cb4d3c0091ebe6fb669
anna4615 <anna4615@hotmail.com>
2021-02-22 14:57:34 +0100

    återställde layout

--- Bakdelar/Startup.cs
+++ Bakdelar/Startup.cs
@@ -19,6 +19,7 @@ namespace Bakdelar
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
             services.AddHttpClient();
             services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddAntiforgery(o => o.HeaderName = "XSRF-TOKEN");
