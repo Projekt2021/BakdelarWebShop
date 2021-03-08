99281e5efc68181287894f51cf1a5cd4e5ba9fd0
jorfus <71066116+jorfus@users.noreply.github.com>
2021-03-08 14:49:09 +0100

    Revert "Cleanup"
    This reverts commit 759ca380fd102efaf9296792c36995a96256346b.

--- Bakdelar/Program.cs
+++ Bakdelar/Program.cs
@@ -13,6 +13,8 @@ namespace Bakdelar
     {
         public static void Main(string[] args)
         {
+
+
             CreateHostBuilder(args).Build().Run();
         }
 
--- Bakdelar_API/Startup.cs
+++ Bakdelar_API/Startup.cs
@@ -29,6 +29,10 @@ namespace Bakdelar_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            CultureInfo ci = new CultureInfo("sv-SE");
+            CultureInfo.DefaultThreadCurrentCulture = ci;
+            CultureInfo.DefaultThreadCurrentUICulture = ci;
+
             services.AddDbContext<BakdelarAppDbContext>
             (options =>
             {
