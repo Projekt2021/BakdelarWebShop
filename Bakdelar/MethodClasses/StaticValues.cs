6178ba9e57f9a494bd00959b9dde8c744139d28e
robinfransson <71066647+robinfransson@users.noreply.github.com>
2021-03-12 16:01:51 +0100

    shipping price added

---
+++ Bakdelar/MethodClasses/StaticValues.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bakdelar.MethodClasses
+{
+    public class StaticValues
+    {
+
+        const decimal shippingFee = 49.00M;
+
+        public static decimal ShippingFee { get { return shippingFee; } }
+    }
+}
