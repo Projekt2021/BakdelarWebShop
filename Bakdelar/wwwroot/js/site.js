f777aee4a75c89cde5717d47384d18b76667984c
anna4615 <anna4615@hotmail.com>
2021-03-15 19:33:39 +0100

    changes in image gallery

--- Bakdelar/wwwroot/js/site.js
+++ Bakdelar/wwwroot/js/site.js
@@ -431,3 +431,4 @@ function getItemCount() {
         restore();
     });
 }
+
--- DataAccess/Migrations/20210312104125_12mars6.Designer.cs
+++ DataAccess/Migrations/20210312104125_12mars6.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(BakdelarAppDbContext))]
-    partial class BakdelarAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210312104125_12mars6")]
+    partial class _12mars6
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
