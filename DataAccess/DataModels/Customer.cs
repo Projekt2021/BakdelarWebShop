f5be60329a7bde735150dae4158e9b3b6df44fa8
tejashree-aj <55463166+tejashree-aj@users.noreply.github.com>
2021-03-22 19:58:52 +0100

    Customer columns set to nullable

--- DataAccess/DataModels/Customer.cs
+++ DataAccess/DataModels/Customer.cs
@@ -11,14 +11,14 @@ namespace DataAccess.DataModels
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CustomerId { get; set; }
-        
-        [Required, MaxLength(500), StringLength(500)]
+
+        [MaxLength(500), StringLength(500)]
         public string UserAddress { get; set; }
 
-        [Required, MaxLength(100), StringLength(100)]
+        [MaxLength(100), StringLength(100)]
         public string FirstName { get; set; }
 
-        [Required, MaxLength(100), StringLength(100)]
+        [MaxLength(100), StringLength(100)]
         public string LastName { get; set; }
 
         public ICollection<Cart> Carts { get; set; }
---
+++ DataAccess/Migrations/20210322185736_CustomerTableFieldsNullable.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class CustomerTableFieldsNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserAddress",
+                table: "Customer",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Customer",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Customer",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserAddress",
+                table: "Customer",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Customer",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Customer",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
