42c1a555fa15d93eaaf21c9882a884e2c737dd8e
tejashree-aj <55463166+tejashree-aj@users.noreply.github.com>
2021-03-23 11:28:56 +0100

    Customer view added in API and UI project - Phone number field added in Customer table.

--- Bakdelar/Classes/CustomerView.cs
+++ Bakdelar/Classes/CustomerView.cs
@@ -1,23 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Bakdelar_API.ViewModels
+namespace Bakdelar.Classes
 {
     public class CustomerView
     {
-     
         public int CustomerId { get; set; }
-
+       
         public Guid UserId { get; set; }
-        
+
+        [Phone]
+        [Display(Name = "Phone number"), Required]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "First Name"), Required]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name"), Required]
         public string LastName { get; set; }
 
+        [Display(Name = "Address"), Required]
         public string Address { get; set; }
-
-        public string PhoneNumber { get; set; }
     }
 }
--- Bakdelar_API/Controllers/CustomerController.cs
+++ Bakdelar_API/Controllers/CustomerController.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Bakdelar_API.ViewModels;
 
 namespace Bakdelar_API.Controllers
 {
@@ -21,10 +24,32 @@ namespace Bakdelar_API.Controllers
             _context = context;
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CustomerView>> Get(string id)
+        {
+            var customer = await _context.Customer.Where(x => x.UserId == Guid.Parse(id)).FirstOrDefaultAsync();
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return new CustomerView
+            {
+                Address = customer.UserAddress,
+                CustomerId = customer.CustomerId,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                PhoneNumber = customer.PhoneNumber,
+                UserId = customer.UserId
+            };
+        }
+
+
         [HttpPost("{id}")]
         public async Task<IActionResult> AddCustomer(string id)
         {
-            if (!_context.Customer.Any(x => x.UserId == Guid.Parse(id)))
+            if (!CustomerExists(id))
             {
                 _context.Customer.Add(new Customer { UserId = Guid.Parse(id) });
                 await _context.SaveChangesAsync();
@@ -32,5 +57,54 @@ namespace Bakdelar_API.Controllers
             return NoContent();
         }
 
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomer(string id, CustomerView customerView)
+        {
+            var customer = _context.Customer.Where(x => x.UserId == Guid.Parse(id)).FirstOrDefault();
+            if (customer == null)
+            {
+                customer = new Customer
+                {
+                    FirstName = customerView.FirstName,
+                    LastName = customerView.LastName,
+                    UserAddress = customerView.Address,
+                    PhoneNumber = customerView.PhoneNumber,
+                    UserId = Guid.Parse(id)
+                };
+                _context.Entry(customer).State = EntityState.Added;
+            }
+            else
+            {
+                customer.FirstName = customerView.FirstName;
+                customer.LastName = customerView.LastName;
+                customer.UserAddress = customerView.Address;
+                customer.PhoneNumber = customerView.PhoneNumber;
+                _context.Entry(customer).State = EntityState.Modified;
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool CustomerExists(string id)
+        {
+            return _context.Customer.Any(e => e.UserId == Guid.Parse(id));
+        }
     }
 }
