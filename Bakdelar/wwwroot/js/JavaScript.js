7306a846ef994574b3f4af56d75150dd4a3b07ac
jorfus <71066116+jorfus@users.noreply.github.com>
2021-03-23 15:00:49 +0100

    Removed unused JS

--- Bakdelar/wwwroot/js/JavaScript.js
+++
@@ -1,4 +0,0 @@
-﻿$.validator.methods.number = function (value, element) {
-    return this.optional(element) || /\d{1,3}(,{1}\d{1,2}){0,1}/.test(value);
-}
-        ///^ -? (?: \d +|\d{ 1, 3 } (?: [\s\.,]\d{ 3 }) +) (?: [\.,]\d +)?$/ 
