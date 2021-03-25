f9f20ea700fcca634b9511e91d7208dc8dec1dce
robinfransson <71066647+robinfransson@users.noreply.github.com>
2021-03-25 09:12:32 +0100

    Update Orders.cshtml.cs

--- Bakdelar/Pages/Orders.cshtml.cs
+++ Bakdelar/Pages/Orders.cshtml.cs
@@ -19,19 +19,29 @@ namespace Bakdelar.Areas.Identity.Pages.Account
 
         public UserManager<MyUser> _userManager { get; set; }
         public AuthenticationDbContext _authDbContext { get; set; }
+        public SignInManager<MyUser> _signInManager { get; set; }
         public OrderDbContext _orderDbContext { get; set; }
-        public OrdersModel(UserManager<MyUser> userManager, AuthenticationDbContext authenticationDbContext, OrderDbContext orderDbContext)
+        public OrdersModel(UserManager<MyUser> userManager, AuthenticationDbContext authenticationDbContext, 
+                           OrderDbContext orderDbContext, SignInManager<MyUser> signInManager)
         {
             _userManager = userManager;
             _authDbContext = authenticationDbContext;
             _orderDbContext = orderDbContext;
+            _signInManager = signInManager;
         }
 
         public void OnGet()
         {
             string userID = _userManager.GetUserId(User);
-            UserOrders = _orderDbContext.Orders.Where(order => order.UserID == userID).ToList();
-
+            var user = _userManager.GetUserAsync(User).Result;
+            if (_userManager.IsInRoleAsync(user, "Admin").Result)
+            {
+                UserOrders = _orderDbContext.Orders.ToList();
+            }
+            else
+            {
+                UserOrders = _orderDbContext.Orders.Where(order => order.UserID == userID).ToList();
+            }
         }
     }
 }
