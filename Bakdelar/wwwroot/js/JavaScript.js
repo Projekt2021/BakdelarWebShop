eb8712b088eac3113ce4d473e74a236b9f41972e
jorfus <71066116+jorfus@users.noreply.github.com>
2021-03-29 15:47:45 +0200

    Tra la la

--- Bakdelar/wwwroot/js/JavaScript.js
+++ Bakdelar/wwwroot/js/JavaScript.js
@@ -1,5 +1,5 @@
 ﻿$.validator.methods.number = function (value, element) {
-    return this.optional(element) || /\d{1,3}(,{1}\d{1,2}){0,1}/.test(value);
+    return this.optional(element)/* || /\d{1,3}(,{1}\d{1,2}){0,1}/.test(value)*/;
 }
 ////$.validator.methods.number = function (value, element) {
 ////    return this.optional(element) || /\d{1,3}(,{1}\d{1,2}){0,1}/.test(value);
