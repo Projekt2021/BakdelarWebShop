dba3eeb4fcb1239a91b17e663fec2e392e746bcc
anna4615 <anna4615@hotmail.com>
2021-04-07 16:42:14 +0200

    minor cleanup in recipes page

--- Bakdelar/Pages/Recipes.cshtml.cs
+++ Bakdelar/Pages/Recipes.cshtml.cs
@@ -18,8 +18,7 @@ namespace Bakdelar.Pages
         public EdamamRecipe Recipe { get; set; }
 
         [BindProperty]
-        [Display(Name = "Ingrediens")]
-        public string Ingredient { get; set; }
+        public string SearchString { get; set; }
 
 
 
@@ -41,7 +40,7 @@ namespace Bakdelar.Pages
 
 
             Task<string> getRecipeStringTask =
-                client.GetStringAsync($"https://api.edamam.com/search?q={Ingredient}&app_id=58e02eb1&app_key=75c99118aeec150db8dabec724cb270f&mealType=snack&from=0&to=1");
+                client.GetStringAsync($"https://api.edamam.com/search?q={SearchString}&app_id=58e02eb1&app_key=75c99118aeec150db8dabec724cb270f&mealType=snack&from=0&to=1");
 
             string recipeString = await getRecipeStringTask;
 
